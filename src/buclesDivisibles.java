15a426b839e3a19cbd8e4064d584b158e32ce461
AlbertNoMes <albertnotariomestres@gmail.com>
2022-01-14 08:58:14 +0100

    Bucles divisibles

---
+++ src/buclesDivisibles.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+public class buclesDivisibles {
+
+	public static void main(String[] args) {
+	
+        //Definimos el bucle, incluye el 100
+        for (int num=1;num<=100;num++){
+            
+        	//Comprobamos si es divisible entre 2 o 3
+            if (num%2==0 || num%3==0){
+                System.out.println(num);
+            }
+        }
+    }
+}
